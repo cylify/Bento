735a0c19a3ced2f35ea1df5fe564adb8dc272139
Adi <82472879+cylify@users.noreply.github.com>
2023-12-19 18:53:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: '798caf31523575e58741eec1bdddb9f9', // Write here your API Key
